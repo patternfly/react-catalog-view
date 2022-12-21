3f1a142652a9299677efa8888bdabc8d58864a12
Austin Sullivan <ausulliv@redhat.com>
2022-12-21 16:30:35 -0500

    feat(release): add release config

---
+++ release.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  branches: [{ name: 'main', channel: 'prerelease' }],
+  analyzeCommits: {
+    preset: 'angular'
+  },
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    ['@semantic-release/npm', { pkgRoot: 'dist' }]
+  ],
+  tagFormat: 'v${version}',
+  dryRun: true
+};
