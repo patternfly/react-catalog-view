fa789f8a486543fa3bf9e9f7f99958711d5ab799
Austin Sullivan <ausulliv@redhat.com>
2023-05-19 13:54:02 -0400

    chore(CI-breaking): test release config from PR

---
+++ relase-config-test/release.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  branches: [
+    { name: 'v4', channel: 'prerelease' },
+    { name: 'main', channel: 'alpha', prerelease: 'alpha' }
+  ],
+  analyzeCommits: {
+    preset: 'angular'
+  },
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'chore', scope: 'CI-release', release: 'patch' },
+          { type: 'chore', scope: 'CI-breaking', release: 'breaking' },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+        pkgRoot: '../packages/module'
+      }
+    ]
+  ],
+  tagFormat: 'prerelease-v${version}'
+};
