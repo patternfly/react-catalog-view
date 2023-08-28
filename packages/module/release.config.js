712dee1e210e44343e8c3391b20ce7abe801b1f6
Austin Sullivan <ausulliv@redhat.com>
2023-08-28 09:16:26 -0400

    chore(ci): Update release and renovate configurations

--- packages/module/release.config.js
+++ packages/module/release.config.js
@@ -1,13 +1,23 @@
 module.exports = {
-  branches: [{ name: 'main', channel: 'prerelease' }],
+  branches: [{ name: 'v4', channel: 'prerelease', range: '4.x' }],
   analyzeCommits: {
     preset: 'angular'
   },
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'chore', scope: 'deps', release: 'patch' },
+          { type: 'chore', scope: 'ci-release', release: 'patch' }
+        ]
+      }
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     '@semantic-release/npm'
   ],
-  tagFormat: 'v${version}'
+  tagFormat: 'v${version}',
+  druRun: true
 };
